fa2b65609b1e94ed16bf5efbd015907024cff476
PRATHAMESH SURVE <134413659+SPrathamesh2611@users.noreply.github.com>
2023-05-24 06:42:35 +0530

    Update Screenshot gui.py
    comment added to the code

--- Screenshot gui.py
+++ Screenshot gui.py
@@ -1,27 +1,45 @@
-#Screen shot GUI
-#from unicodedata import name
+# Screen shot GUI
+
 import pyautogui
 import time
 import tkinter as tk
 
+# Define a function to capture a screenshot
 def screenshot():
+    """
+    This function captures a screenshot of the screen and saves it to a file.
+    It uses PyAutoGUI library to take the screenshot and tkinter for GUI components.
+    """
+    # Wait for 5 seconds to allow switching to the desired screen
     time.sleep(5)
+    
+    # Generate a unique filename based on the current timestamp
     name = time.time()
     name = 'C:/Users/hp/Documents/github/{}.png'.format(name)
+    
+    # Capture the screenshot using PyAutoGUI
     img = pyautogui.screenshot()
+    
+    # Save the screenshot to the specified file path
     img.save(name)
+    
+    # Display the screenshot using the default image viewer
     img.show()
 
-
-
+# Create a tkinter GUI window
 root = tk.Tk()
+
+# Create a frame within the window
 frame = tk.Frame(root)
 frame.pack()
 
-button = tk.Button(frame,text="Take Screenshot",command=screenshot)
+# Create a button for capturing a screenshot
+button = tk.Button(frame, text="Take Screenshot", command=screenshot)
 button.pack(side=tk.LEFT)
 
-close = tk.Button(frame,text="Quit",command=quit)
+# Create a button for quitting the application
+close = tk.Button(frame, text="Quit", command=quit)
 close.pack(side=tk.LEFT)
 
+# Start the GUI event loop
 root.mainloop()
