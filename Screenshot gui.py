55b2e8388baaf2e1694461e5354b92f38cd29c3a
PRATHAMESH SURVE <134413659+SPrathamesh2611@users.noreply.github.com>
2023-05-24 06:41:01 +0530

    code file added

---
+++ Screenshot gui.py
@@ -0,0 +1,27 @@
+#Screen shot GUI
+#from unicodedata import name
+import pyautogui
+import time
+import tkinter as tk
+
+def screenshot():
+    time.sleep(5)
+    name = time.time()
+    name = 'C:/Users/hp/Documents/github/{}.png'.format(name)
+    img = pyautogui.screenshot()
+    img.save(name)
+    img.show()
+
+
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame,text="Take Screenshot",command=screenshot)
+button.pack(side=tk.LEFT)
+
+close = tk.Button(frame,text="Quit",command=quit)
+close.pack(side=tk.LEFT)
+
+root.mainloop()
